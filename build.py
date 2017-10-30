df5d84b1dd0bcbff643c6efe43edfdec2a4e1ad1
Alexander Zaitsev <zamazan4ik@tut.by>
2017-10-31 01:59:29 +0300

    Disable pure C

--- build.py
+++ build.py
@@ -3,5 +3,5 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(args="--build missing")
-    builder.add_common_builds(shared_option_name="libjpeg:shared", pure_c=True)
+    builder.add_common_builds()
     builder.run()
